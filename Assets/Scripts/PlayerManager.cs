dce2ba4ace46a8df1c142c0793e85c0c76b12461
john ayudante <john.ayudante@tecsup.edu.pe>
2025-09-24 17:48:03 -0500

    segudno avance

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerManager : MonoBehaviour
 {
+    Rigidbody2D rb;
+    [SerializeField] private float speed;
+    private Vector2 Movement;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        rb= GetComponent<Rigidbody2D>();
+    }
+    private void OnEnable()
+    {
+        ImputSystem.OnMove += Movement2D;
+    }
+    private void OnDisable()
+    {
+        ImputSystem.OnMove -= Movement2D;   
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        rb.linearVelocity= new Vector2 (Movement.x,Movement.y);
+    }
+    private void Movement2D(Vector2 direction)
+    {
+        Movement = direction;
     }
 }
